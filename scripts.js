0d5287c03d8ea4f3382c2821351e82851397ed0c
krjone26 <krjone26@asu.edu>
2024-10-01 14:41:26 -0700

    Update js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,27 @@ let images = document.getElementsByTagName("img");
 for(let image of images){
     image.width = "250";
 }
+
+// selecting by class name
+let ltBlueSpans = document.getElementsByClassName("light-blue");
+let medBlueSpans = document.getElementsByClassName("med-blue");
+// console.log(ltBlueSpans);
+
+for(let span of ltBlueSpans){
+    span.style.color = "#7C9EA6";
+}
+
+for(let span of medBlueSpans){
+    span.style.color = "#3C5E73";
+}
+
+// selecting many with a css selector
+// array of colors
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+
+let svgs = document.querySelectorAll("#svgs svg");
+
+for(let i = 0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
